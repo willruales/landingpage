c3e294fe8eab791427b056dd172565dbd249b364
William Ruales <ruales@hotmail.co.uk>
2022-10-14 10:11:53 +0100

    color additions

--- javascript.js
+++ javascript.js
@@ -19,3 +19,33 @@ links.forEach((links) => {
     });
 
 })
+
+const img = document.querySelectorAll('.aboutme img')
+console.log(img);
+
+img.forEach((img) => {
+    img.addEventListener('mouseover', () => {
+        img.classList.add('fader')
+    })
+
+    img.addEventListener('mouseout', () => {
+        img.classList.remove('fader')
+    })
+})
+////////////////////
+
+let div = document.querySelectorAll('.aboutme img')
+console.log(div);
+
+div.forEach((div) => {
+    let section = document.createElement("p");
+    let sectiontext = document.createTextNode("please workkkkkkkkkkkkkkkkkkkkkkkkk");
+    section.appendChild(sectiontext);
+    div.appendChild(section);
+})
+///////////////////
+
+// let chose = document.querySelector('.group3')
+// console.log(chose)
+// // let delete = document.createElement("p1");
+// // let deleteme = document.createTextNode("added");
